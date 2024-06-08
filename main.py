de57b68a93dfe545754108895c504c68ee6a480d
PedroHenriqueCruzDaSilva <samuraigamer100000@gmail.com>
2024-06-08 15:45:24 -0300

    Primeiro Comando

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-# Our main file.
+#!/usr/bin/env python3
 
-import speech_recognition as sr
+from vosk import Model, KaldiRecognizer
+import os
+import pyaudio
+import pyttsx3
+import json
+import core
 
-# Cria um Reconhecedor
-r = sr.Recognizer()
+# Síntese de fala
+engine = pyttsx3.init()
 
-# Abrir o Microfone para captura de audio
-with sr.Microphone() as source:
-  while True:
-    audio = r.listen(source) # Define Microfone como fonte de áudio
+#voices = engine.getProperty('voices')
+#engine.setProperty('voice', voices[-1].id)
 
-    print(r.recognize_google(audio, language='pt'))
+def speak(text):
+  engine.say(text)
+  engine.runAndWait()
+
+# Reconhecimento de fala
+
+model = Model("model")
+rec = KaldiRecognizer(model, 16000)
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
+
+# Loop do reconhecimento de fala
+while True:
+  data = stream.read(2048)
+  if len(data) ==0:
+    break
+  if rec.AcceptWaveform(data):
+    result = rec.Result()
+    result = json.loads(result)
+
+    if result is not None:
+       text = result['text']
+
+       if text == "que horas são" or text == "me diga as horas":
+         speak(core.SystemInfo.get_time())
